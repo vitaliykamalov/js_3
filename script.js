401d77e9c8031f1e3e4a6f0c72c3ea22db18d05e
Vitaliy <kamalovvitaliy601@gmail.com>
2020-01-22 15:12:24 +0500

    js_3

---
+++ script.js
@@ -0,0 +1,81 @@
+var time = +prompt('Введите время суток');
+switch(time) {
+    case 1:
+        alert('1 час ночи');
+    break; 
+    case 2:
+        alert('2 часа ночи');
+    break; 
+    case 3:
+        alert('3 часа ночи');
+    break; 
+    case 4:
+        alert('4 часа ночи');
+    break; 
+    case 5:
+        alert('5 часов утра');
+    break; 
+    case 6:
+        alert('6 часов утра');
+    break; 
+    case 7:
+        alert('7 часов утра');
+    break; 
+    case 8:
+        alert('8 часов утра');
+    break; 
+    case 9:
+        alert('9 часов утра');
+    break; 
+    case 10:
+        alert('10 часов дня');
+    break; 
+    case 11:
+        alert('11 часов дня');
+    break; 
+    case 12:
+        alert('12 часов дня');
+    break; 
+    case 13:
+        alert('1 час дня');
+    break; 
+    case 14:
+        alert('2 часа дня');
+    break; 
+    case 15:
+        alert('3 часа дня');
+    break; 
+    case 16:
+        alert('4 часа дня');
+    break; 
+    case 17:
+        alert('5 часов вечера');
+    break; 
+    case 18:
+        alert('6 часов вечера');
+    break; 
+    case 19:
+        alert('7 часов вечера');
+    break; 
+    case 20:
+        alert('8 часов вечера');
+    break; 
+    case 21:
+        alert('9 часов вечера');
+    break; 
+    case 22:
+        alert('10 часов вечера');
+    break; 
+    case 22:
+        alert('10 часов вечера');
+    break; 
+    case 23:
+        alert('11 часов ночи');
+    break; 
+    case 0:
+        alert('12 часов ночи');
+    break; 
+    default:
+        alert('Такого времени суток нет')
+    break;
+}
